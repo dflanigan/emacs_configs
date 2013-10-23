02ffcae7fc9eeb6b2df5a3a51f5ae1f56f59dc02
Dennis Flanigan <d.flanigan@f5.com>
2013-10-23 16:49:20 -0600

    Moving manualing installed .el files out of the way of the package installed .el files

--- dot_emacs.el
+++ dot_emacs.el
@@ -1,18 +1,23 @@
-
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives
+	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives
+	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+)
 
 ;; Load python-mode for scons files
 (setq auto-mode-alist(cons '("SConstruct" . python-mode) auto-mode-alist))
 (setq auto-mode-alist(cons '("SConscript" . python-mode) auto-mode-alist))
 
 ;; Robot mode settings
-(load-file "~/.emacs.d/robot-mode.el")
+(load-file "~/emacs_config/manual_install/robot-mode.el")
 (add-to-list 'auto-mode-alist '("\\.robot\\'" . robot-mode))
 
 ;; Google Protobuf mode
-(load-file "~/.emacs.d/protobuf-mode.el")
+(load-file "~/emacs_config/manual_install/protobuf-mode.el")
 (add-to-list 'auto-mode-alist '("\\.tlc\\'" . protobuf-mode))
-
-
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
