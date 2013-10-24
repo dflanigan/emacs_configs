37dc0beb61a5de1d14e2eb37b66a362ba6e52ed2
Dennis Flanigan <d.flanigan@f5.com>
2013-10-24 12:02:33 -0600

    Work on python configs

--- dot_emacs.el
+++ dot_emacs.el
@@ -25,13 +25,83 @@
 (require 'magit)
 (global-set-key "\C-xg" 'magit-status)
 
+;; auto-pair
+(require 'autopai
+;; yasnippet
+(require 'yasnippet)
 
-;; Load python-mode for scons files
 ;; ;; Python mode settings
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
 (setq auto-mode-alist(cons '("SConstruct" . python-mode) auto-mode-alist))
 (setq auto-mode-alist(cons '("SConscript" . python-mode) auto-mode-alist))
+(setq py-electric-colon-active t)
+(add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'python-mode-hook 'yas-minor-mode)
+
+;; auto-complete mode extra settings
+(require 'auto-complete)
+(setq
+ ac-auto-start 2
+ ac-override-local-map nil
+ ac-use-menu-map t
+ ac-candidate-limit 20)
+
+(add-hook 'python-mode-hook 'auto-complete-mode)
+
+;; ;; Jedi settings
+(require 'jedi)
+;; It's also required to run "pip install --user jedi" and "pip
+;; install --user epc" to get the Python side of the library work
+;; correctly.
+;; With the same interpreter you're using.
+
+;; if you need to change your python intepreter, if you want to change it
+;; (setq jedi:server-command
+;;       '("python2" "/home/andrea/.emacs.d/elpa/jedi-0.1.2/jediepcserver.py"))
+(add-hook 'python-mode-hook
+	  (lambda ()
+	    (jedi:setup)
+	    (jedi:ac-setup)
+            (local-set-key "\C-cd" 'jedi:show-doc)
+            (local-set-key (kbd "M-SPC") 'jedi:complete)
+            (local-set-key (kbd "M-.") 'jedi:goto-definition)))
+
+;; Flymake settings for Python
+;; for syntax checking.
+;; (require 'flymake)
+;; (defun flymake-python-init ()
+;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
+;;                      'flymake-create-temp-inplace))
+;;          (local-file (file-relative-name
+;;                       temp-file
+;;                       (file-name-directory buffer-file-name))))
+;;     (list "epylint" (list local-file))))
+
+;; (defun flymake-activate ()
+;;   "Activates flymake when real buffer and you have write access"
+;;   (if (and
+;;        (buffer-file-name)
+;;        (file-writable-p buffer-file-name))
+;;       (progn
+;;         (flymake-mode t)
+;;         ;; this is necessary since there is no flymake-mode-hook...
+;;         (local-set-key (kbd "C-c n") 'flymake-goto-next-error)
+;;         (local-set-key (kbd "C-c p") 'flymake-goto-prev-error))))
+
+;; (defun ca-flymake-show-help ()
+;;   (when (get-char-property (point) 'flymake-overlay)
+;;     (let ((help (get-char-property (point) 'help-echo)))
+;;       (if help (message "%s" help)))))
+
+;; (add-hook 'post-command-hook 'ca-flymake-show-help)
+
+;; (add-to-list 'flymake-allowed-file-name-masks
+;;              '("\\.py\\'" flymake-python-init))
+
+;; (add-hook 'python-mode-hook 'flymake-activate)
+
+
 
 ;; Robot mode settings
 (load-file "~/emacs_config/manual_install/robot-mode.el")
@@ -40,6 +110,9 @@
 ;; Google Protobuf mode
 (load-file "~/emacs_config/manual_install/protobuf-mode.el")
 (add-to-list 'auto-mode-alist '("\\.tlc\\'" . protobuf-mode))
+
+;; Interactively Do Things
+(ido-mode t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
