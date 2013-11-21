766328f845da724584fc24cbd2c1b8c7d3e6a64a
Dennis Flanigan <d.flanigan@f5.com>
2013-11-21 14:56:51 -0700

    Turned of startup screen

--- dot_emacs.el
+++ dot_emacs.el
@@ -99,6 +99,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(indent-tabs-mode nil)
+ '(inhibit-startup-screen t)
  '(make-backup-files nil)
  '(tool-bar-mode nil))
 (custom-set-faces
